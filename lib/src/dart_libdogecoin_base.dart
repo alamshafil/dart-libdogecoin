b5168099eed349a450ab365b0dc5553b13969f09
alamshafil <alamshafil@pm.me>
2022-08-12 16:55:16 -0400

    Add comments

--- lib/src/dart_libdogecoin_base.dart
+++ lib/src/dart_libdogecoin_base.dart
@@ -3,12 +3,15 @@ import 'package:ffi/ffi.dart';
 
 import '../libdogecoin_bindings.dart' as libdogecoin;
 
+/// Low-level libdogecoin bindings.
 final rawLibdogecoin =
     libdogecoin.libdogecoin(DynamicLibrary.open("libdogecoin.so"));
 
+/// High-level libdogecoin bindings.
 class LibDogecoin {
   // Address API
 
+  /// Generates a private and public keypair (a wallet import format private key and a p2pkh ready-to-use corresponding dogecoin address)
   static Keypair generatePrivPubKeypair(bool isTestnet) {
     int testnet = isTestnet ? 1 : 0;
     Pointer<Char> privKey = malloc.allocate(53);
@@ -29,6 +32,7 @@ class LibDogecoin {
     return pair;
   }
 
+  /// Generates a hybrid deterministic WIF master key and p2pkh ready-to-use corresponding dogecoin address.
   static HDKeypair generateHDMasterPubKeypair(bool isTestnet) {
     int testnet = isTestnet ? 1 : 0;
     Pointer<Char> masterPrivKey = malloc.allocate(200);
@@ -50,6 +54,7 @@ class LibDogecoin {
     return pair;
   }
 
+  /// Generates a new dogecoin address from a HD master key.
   static String generateDerivedHDPubkey(String masterPrivKey) {
     Pointer<Char> childPubKey = malloc.allocate(35);
 
@@ -67,6 +72,7 @@ class LibDogecoin {
     return result;
   }
 
+  /// Verify that a private key and dogecoin address match.
   static bool verifyPrivPubKeypair(
       String privKey, String pubkey, bool isTestnet) {
     int testnet = isTestnet ? 1 : 0;
@@ -83,6 +89,7 @@ class LibDogecoin {
     return result == 0 ? false : true;
   }
 
+  /// Verify that a HD Master key and a dogecoin address matches.
   static bool verifyHDMasterPubKeypair(
       String privKeyMaster, String pubkeyMaster, bool isTestnet) {
     int testnet = isTestnet ? 1 : 0;
@@ -99,6 +106,7 @@ class LibDogecoin {
     return result == 0 ? false : true;
   }
 
+  /// Verify that a dogecoin address is valid.
   static bool verifyP2pkhAddress(String p2pkhPubkey) {
     int result = rawLibdogecoin.verifyP2pkhAddress(
         p2pkhPubkey.toNativeUtf8().cast<Char>(), p2pkhPubkey.length);
@@ -108,15 +116,22 @@ class LibDogecoin {
 
   // Transaction API
 
+  /// Create a new dogecoin transaction
+  ///
+  /// Returns the [txindex] in memory of the transaction being worked on.
   static int startTransaction() {
     int result = rawLibdogecoin.start_transaction();
     return result;
   }
 
-  static void clearTransaction(int index) {
-    rawLibdogecoin.clear_transaction(index);
+  /// Clear the transaction at (txindex) in memory.
+  static void clearTransaction(int txindex) {
+    rawLibdogecoin.clear_transaction(txindex);
   }
 
+  /// Add a utxo to the transaction being worked on at [txindex] specifying the utxo's txid and vout.
+  ///
+  /// Returns true if successful.
   static bool addUtxo(int txindex, String hexUtxo, int vout) {
     int result = rawLibdogecoin.add_utxo(
         txindex, hexUtxo.toNativeUtf8().cast<Char>(), vout);
@@ -124,6 +139,9 @@ class LibDogecoin {
     return result == 0 ? false : true;
   }
 
+  /// Add an output to the transaction being worked on at [txindex] of amount [vout] in dogecoins.
+  ///
+  /// Returns true if successful.
   static bool addOutput(int txindex, String destinationaddress, String amount) {
     int result = rawLibdogecoin.add_output(
         txindex,
@@ -133,6 +151,8 @@ class LibDogecoin {
     return result == 0 ? false : true;
   }
 
+  /// Finalize the transaction being worked on at [txindex], with the [destinationaddress] paying a fee of [subtractedfee],
+  /// re-specify the amount in dogecoin for verification, and change address for change. If not specified, change will go to the first utxo's address.
   static String finalizeTransaction(
       int txindex,
       String destinationaddress,
@@ -150,6 +170,7 @@ class LibDogecoin {
     return result;
   }
 
+  /// Retrieve the raw transaction at [txindex] as a hex string.
   static String getRawTransaction(int txindex) {
     Pointer<Char> rawResult = rawLibdogecoin.get_raw_transaction(txindex);
 
@@ -157,6 +178,7 @@ class LibDogecoin {
     return result;
   }
 
+  /// **DO NOT USE** Sign a raw transaction hexadecimal string using inputindex, scripthex, sighashtype and privkey.
   // TODO: Fix signRawTransaction() returns random invalid two UTF-8 chars at end.
   static String signRawTransaction(int inputindex, String incomingrawtx,
       String scripthex, int sighashtype, String privkey) {
@@ -187,6 +209,7 @@ class LibDogecoin {
     return strResult;
   }
 
+  /// Sign a raw transaction in memory at [txindex], sign [inputindex] with [scripthex] of [sighashtype], with [privkey].
   static bool signTransaction(
       int txindex, String scriptPubkey, String privkey) {
     rawLibdogecoin.dogecoin_ecc_start();
@@ -201,6 +224,7 @@ class LibDogecoin {
     return result == 0 ? false : true;
   }
 
+  /// Store a raw transaction that's already formed, and give it a txindex in memory. (txindex) is returned as int.
   static int storeRawTransaction(String incomingrawtx) {
     int result = rawLibdogecoin
         .store_raw_transaction(incomingrawtx.toNativeUtf8().cast<Char>());
@@ -209,14 +233,24 @@ class LibDogecoin {
   }
 }
 
+/// A class to hold a private key [privKey] and public key [pubKey].
 class Keypair {
+  /// Private key
   late String privKey;
+
+  /// Public key
   late String pubKey;
+
   Keypair(this.privKey, this.pubKey);
 }
 
+/// A class to hold a HD master private key [masterPrivKey] and HD master public key [masterPubKey].
 class HDKeypair {
+  /// HD master private key
   late String masterPrivKey;
+
+  // HD master public key
   late String masterPubKey;
+
   HDKeypair(this.masterPrivKey, this.masterPubKey);
 }
